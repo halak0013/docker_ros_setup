2ee2231d8401f790390d892a1e28bf94b5b55b2a
Muhammet <halakmuhammet145@gmail.com>
2023-02-27 15:20:51 +0300

    insatall and open ros melodic docker

---
+++ without_nvidia/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+kullanici=$(whoami)
+
+sudo apt install docker.io
+docker build -t ros_ehli .
+mkdir ~/ros
+
+docker run -it -e DISPLAY=$DISPLAY -e PULSE_SERVER=127.0.0.1 -v /dev/dri:/dev/dri -v /home/$kullanici/ros:/home --net host -v /tmp/.X11-unix:/tmp/.X11-unix --name ros_sistemi ros_ehli
